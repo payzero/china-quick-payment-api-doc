464dbb57eb3cb41e6b0a8371bc1adefe86baf449
Raphael HUANG <raph.huang@transtan.com>
2019-04-21 15:54:49 +0800

    增加demo

--- src/main/java/cn/payzero/quickpaysdk/Application.java
+++ src/main/java/cn/payzero/quickpaysdk/Application.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	
+	//启动本应用后 （命令行启动命令为 mvn spring-boot:run)
+	//可通过HTTP请求此地址 http://127.0.0.1:9030/test?testAPIName= 并携带不通参数，来测试不同接口
+	//例如请求 http://127.0.0.1:9030/test?testAPIName=createAndPay 可用于测试创建并支付订单接口
+	
 	public static void main(String args[]) throws Exception {
 		SpringApplication.run(Application.class, args);
 	}
